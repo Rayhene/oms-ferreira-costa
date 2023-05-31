9ca36745a6ab089a624874ce83f235c25aac3beb
Rafael <rafaelmsfigueiroa@gmail.com>
2023-05-31 16:30:14 -0300

    feat: client informations and back button

--- front/src/components/StatusPedido/index.jsx
+++ front/src/components/StatusPedido/index.jsx
@@ -4,7 +4,7 @@ import {
 import { Box, Heading } from '@chakra-ui/react'
 import { WarningTwoIcon } from '@chakra-ui/icons'
 import { useState, useEffect } from 'react'
-import { buscarTodosPedidos } from "../../services/api";
+import { buscarTodosPedidos } from "../../services/api"
 
 const steps = [
   { title: 'Pedido Realizado', description: 'Date & Time' },
@@ -36,33 +36,32 @@ function Example() {
   }, []);
 
   return (
-    <Box display="flex" justifyContent='center' alignItems="center" height="100vh" width="100vw"
-      border='1px solid gray'>
-      <Box display="flex" flexDirection="column" justifyContent='center' alignItems="center" height="85vh" width="45vh"
-        border='1px solid gray' marginRight="65px" marginLeft="auto" borderRadius="8px" boxShadow="0 0 10px rgba(0, 0, 0, 0.1)">
-        <Heading as="h1" size="sm" marginBottom="20px">Histórico do Status do Pedido</Heading>
-        <Stepper index={activeStep} colorScheme='green' orientation='vertical' height='460px' gap='0' size='sm'>
-          {steps.map((step, index) => (
-            <Step key={index}>
-              <StepIndicator>
-                <StepStatus
-                  complete={index < 8 && <StepIcon />}
-                  active={<StepNumber />}
-                  incomplete={index === 8 && <WarningTwoIcon color="red.500" />}
-                  //stepError={index === 8 && <WarningTwoIcon color="red.500" />}
-                />
-              </StepIndicator>
+    <Box display="flex" flexDirection="column" justifyContent='center' alignItems="center" height="85vh" width="45vh"
+      border='1px solid gray' marginRight="0px" marginLeft="6px" borderRadius="8px" boxShadow="0 0 10px rgba(0, 0, 0, 0.1)">
+      <Heading as="h1" size="md" marginBottom="20px">Histórico do Status do Pedido</Heading>
+      <Stepper index={activeStep} colorScheme='green' orientation='vertical' height='460px' gap='0' size='sm'>
+        {steps.map((step, index) => (
+          <Step key={index}>
+            <StepIndicator>
+              <StepStatus
+                complete={index < 8 && <StepIcon />}
+                active={<StepNumber />}
+                incomplete={index === 8 && <WarningTwoIcon color="red.500" />}
+                //stepError={index === 8 && <WarningTwoIcon color="red.500" />}
+              />
+            </StepIndicator>
 
-              <Box flexShrink='0'>
-                <StepTitle style={index === 8 ? { color: 'red' } : null} >{step.title}</StepTitle>
-                <StepDescription style={index === 8 ? { color: 'red' } : null} >{step.description}</StepDescription>
-              </Box>
+            <Box flexShrink='0'>
+              <StepTitle style={index === 8 ? { color: 'red' } : null} >{step.title}</StepTitle>
+              <StepDescription style={index === 8 ? { color: 'red' } : null} >{step.description}</StepDescription>
+            </Box>
 
-              <StepSeparator />
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
+            <StepSeparator />
+          </Step>
+          
+        ))}
+      </Stepper>
+      
     </Box>
   )
 }
