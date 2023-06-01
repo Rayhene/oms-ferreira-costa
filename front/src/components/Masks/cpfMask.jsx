4aec7de5004a513148254d8dc8f9485bf954f65b
Igor Félix <igor.pereira@avenuecode.com>
2023-06-01 16:23:16 -0300

    feat: add mask in phone and cpf

---
+++ front/src/components/Masks/cpfMask.jsx
@@ -0,0 +1,9 @@
+export const cpfMask = (value) => {
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+}
+export default cpfMask;
