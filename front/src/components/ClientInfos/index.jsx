d8cd44969f10b09453c0f19fefef2f5f5cf69841
MarcosTenorio <marcosvtenoriodev@gmail.com>
2023-06-02 15:58:31 -0300

    feat: add animation while user wait API data

--- front/src/components/ClientInfos/index.jsx
+++ front/src/components/ClientInfos/index.jsx
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { buscarPedidoPorNumero } from "../../services/api";
-import { Box, Text, Heading, Flex } from "@chakra-ui/react";
+import { Box, Text, Heading, Flex, Table, Tr, Td } from "@chakra-ui/react";
 import cpfMask from "../Masks/cpfMask";
 import phoneMask from "../Masks/phoneMask";
+import Loading from "../Loading";
 
 const ClientInfos = () => {
     const [clientData, setClientData] = useState(null);
     const params = useParams();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         buscarPedidoPorNumero(params.pedidoId)
             .then((data) => {
                 console.log('data', data);
                 getClientData(data);
+                setIsLoading(false);
             })
             .catch((error) => {
                 console.error('Erro ao buscar todos os pedidos:', error);
@@ -36,7 +39,16 @@ const ClientInfos = () => {
         boxShadow="base"
         border="1px"
         borderColor="gray.300" 
-        borderRadius="8">
+        borderRadius="8">{isLoading ? (
+            <Table>
+                <Tr>
+                    <Td colSpan={6} py="10px">
+                        <Loading mb={3} mt={3} />
+                    </Td>
+                </Tr>
+            </Table>
+          ) : (
+            <>
             <Box w="50%">
                 <Box h="55%">
                     <Heading as="h2" fontSize="lg" h="30%">Cliente</Heading>
@@ -73,6 +85,7 @@ const ClientInfos = () => {
                     )}
                 </Box>
             </Box>
+            </>)};
         </Flex>
     );
 }
