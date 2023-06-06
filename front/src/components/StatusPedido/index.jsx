cee23729ee9d545d42e673111d6a1f232e5b48c3
Carlos <carlossergiosilvacosta@gmail.com>
2023-06-06 06:55:06 -0300

    feat updating order status page description

--- front/src/components/StatusPedido/index.jsx
+++ front/src/components/StatusPedido/index.jsx
@@ -1,23 +1,24 @@
 import {
   Step, StepDescription, StepIcon, StepIndicator, StepNumber, StepSeparator, StepStatus, StepTitle,
-  Stepper, useSteps
+  Stepper, Table, Td, Tr, useSteps
 } from '@chakra-ui/react'
 import { Box, Heading } from '@chakra-ui/react'
 import { WarningIcon } from '@chakra-ui/icons'
 import { useState, useEffect } from 'react'
 import { buscarPedidoPorNumero } from '../../services/api'
+import Loading from '../Loading'
 
 
 const steps = [
-  { title: 'Pedido Realizado', description: 'Date & Time' },
-  { title: 'Captura', description: 'Date & Time' },
-  { title: 'Anti-Fraude', description: 'Date & Time' },
-  { title: 'Faturado', description: 'Date & Time' },
-  { title: 'E-mail Enviado ao Cliente', description: 'Date & Time' },
-  { title: 'Picking', description: 'Date & Time' },
-  { title: 'Empacotamento e Designição', description: 'Date & Time' },
-  { title: 'Entregue para Transportadora', description: 'Date & Time' },
-  { title: 'Produto Entregue', description: 'Date & Time' }
+  { title: 'Pedido Realizado', description: '' },
+  { title: 'Captura', description: '' },
+  { title: 'Anti-Fraude', description: '' },
+  { title: 'Faturado', description: '' },
+  { title: 'E-mail Enviado ao Cliente', description: '' },
+  { title: 'Picking', description: '' },
+  { title: 'Empacotamento e Designição', description: '' },
+  { title: 'Entregue para Transportadora', description: '' },
+  { title: 'Produto Entregue', description: '' }
 ]
 
 let indexStatus = 0;
@@ -25,16 +26,18 @@ let indexStatus = 0;
 function Example() {
 
   const [pedido, setPedido] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
   const [statusErro, setStatusErro] = useState(false);
-  const [dataDaCompra, setDataDaCompra] = useState("");
+  const [datasDescricao, setdatasDescricao] = useState([]);
 
   useEffect(() => {
     buscarPedidoPorNumero(buscarNumeroPedido())
       .then((data) => {
         setPedido(data);
+        setIsLoading(false);
         setStatusErro(data?.status_erro || false);
-        setDataDaCompra(data?.dataDaCompra || "");
-        
+        setdatasDescricao(datas);
+
       })
       .catch((error) => {
         console.error("Erro ao buscar todos os pedidos:", error);
@@ -45,6 +48,7 @@ function Example() {
     let numeroPedido = window.location.pathname.split('/');
     return numeroPedido[2];
   };
+
 
   if (pedido?.status_pedido === "PEDIDO REALIZADO") {
     indexStatus = Number(0);
@@ -86,6 +90,16 @@ function Example() {
     setActiveStep(indexStatus);
   }, [indexStatus]);
 
+  useEffect(() => {
+  const datas = steps.map((step, index) => {
+    if (index <= indexStatus) {
+      return pedido?.dataDaCompra;
+    }
+    return null;
+  });
+  setdatasDescricao(datas);
+},[pedido, indexStatus]);
+
   return (
     <Box
       display="flex"
@@ -93,38 +107,45 @@ function Example() {
       justifyContent='center'
       alignItems='center'
       padding="20px"
-      //height="85vh"
-      //width="96%"
-      //ml="75%"
       border='1px solid gray'
       marginRight="9px"
       marginLeft="9px"
       borderRadius="8px"
       gap={'2vh'}
-      >
+    >
       <Heading as="h1" size="md" marginBottom="20px" fontSize="18px">Histórico do Status do Pedido</Heading>
-      <Stepper index={activeStep} colorScheme='green' orientation='vertical' height='460px' gap='0' size='sm'>
-        {steps.map((step, index) => (
-          <Step key={index}>
-            <StepIndicator>
-              <StepStatus
-                complete={<StepIcon />}
-                active={statusErro || indexStatus === 8 ? <WarningIcon color="red.500" /> : <StepNumber />}
-                incomplete={<StepNumber />}
-                //active={StatusErro === "true" ? (<WarningIcon color="red.500" />) : (<StepNumber />)}
-              />
-            </StepIndicator>
+      {isLoading ? (
+        <Table>
+          <Tr>
+            <Td colSpan={6} py="10px">
+              <Loading mb={3} mt={3} />
+            </Td>
+          </Tr>
+        </Table>
+      ) : (
+        <>
+          <Stepper index={activeStep} colorScheme='green' orientation='vertical' height='460px' gap='0' size='sm'>
+            {steps.map((step, index) => (
+              <Step key={index}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<StepIcon />}
+                    active={statusErro || indexStatus === 8 ? <WarningIcon color="red.500" /> : <StepNumber />}
+                    incomplete={<StepNumber />}
+                  />
+                </StepIndicator>
 
-            <Box flexShrink='0'>
-              <StepTitle style={((statusErro && activeStep === index) || (indexStatus === 8 && activeStep === index)) ? { color: 'red' } : null} >{step.title}</StepTitle>
-              <StepDescription >{step.description}</StepDescription>
-            </Box>
+                <Box flexShrink='0'>
+                  <StepTitle style={((statusErro && activeStep === index) || (indexStatus === 8 && activeStep === index)) ? { color: 'red' } : null} >{step.title}</StepTitle>
+                  <StepDescription >{datasDescricao[index]}</StepDescription>
+                </Box>
 
-            <StepSeparator />
-          </Step>
+                <StepSeparator />
+              </Step>
 
-        ))}
-      </Stepper>
+            ))}
+          </Stepper>
+        </>)}
     </Box>
   )
 }
