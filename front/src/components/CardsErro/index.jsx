c19a40357de925e5c41150659b791e4b19864eab
Igor Félix <igor.pereira@avenuecode.com>
2023-06-06 22:06:43 -0300

    feat: leaving the error cards dynamic

--- front/src/components/CardsErro/index.jsx
+++ front/src/components/CardsErro/index.jsx
@@ -17,7 +17,12 @@ function CardsErro() {
       .catch((error) => {
         console.error("Erro ao buscar todos os pedidos:", error);
       });
-  }, []);
+  }, []); 0
+
+  const getQuantidadePedidosErro = (status) => {
+    return pedidos.filter(pedido => pedido.status_pedido === status && pedido.status_erro === true).length;
+  };
+
 
   return (
     <Box marginTop='4rem'>
@@ -30,16 +35,16 @@ function CardsErro() {
           <Flex justifyContent={['center', 'flex-start']} gap='1rem' flexWrap='wrap'>
             <Box minHeight="10vh" width={['100%', '26vh']} border='1.10733px solid rgba(158, 158, 158, 0.7)' borderRadius='6.23432px' padding='5px'>
               <Heading as="h1" size="sm" margin="8px 18px">Anti-Fraude</Heading>
-              <Flex alignItems="center" justifyContent={['space-between']}margin="16px 18px">
-                <Heading as="h1" fontSize="2xl">48</Heading>
+              <Flex alignItems="center" justifyContent={['space-between']} margin="16px 18px">
+                <Heading as="h1" fontSize="2xl">{getQuantidadePedidosErro("ANTIFRAUDE")}</Heading>
                 <Image src={img} width="20px" height="20px" />
               </Flex>
             </Box>
 
             <Box minHeight="10vh" width={['100%', '26vh']} border='1.10733px solid rgba(158, 158, 158, 0.7)' borderRadius='6.23432px' padding='5px'>
               <Heading as="h1" size="sm" margin="8px 18px">Captura</Heading>
-              <Flex alignItems="center" justifyContent={['space-between']}margin="16px 18px">
-                <Heading as="h1" fontSize="2xl">354</Heading>
+              <Flex alignItems="center" justifyContent={['space-between']} margin="16px 18px">
+                <Heading as="h1" fontSize="2xl">{getQuantidadePedidosErro("CAPTURA")}</Heading>
                 <Image src={img} width="20px" height="20px" />
               </Flex>
             </Box>
@@ -47,8 +52,8 @@ function CardsErro() {
 
             <Box minHeight="10vh" width={['100%', '26vh']} border='1.10733px solid rgba(158, 158, 158, 0.7)' borderRadius='6.23432px' padding='5px'>
               <Heading as="h1" size="sm" margin="8px 18px">Carrinho</Heading>
-              <Flex alignItems="center" justifyContent={['space-between']}margin="16px 18px">
-                <Heading as="h1" fontSize="2xl">77</Heading>
+              <Flex alignItems="center" justifyContent={['space-between']} margin="16px 18px">
+                <Heading as="h1" fontSize="2xl">{getQuantidadePedidosErro("FATURADO")}</Heading>
                 <Image src={img} width="20px" height="20px" />
               </Flex>
             </Box>
@@ -56,8 +61,8 @@ function CardsErro() {
 
             <Box minHeight="10vh" width={['100%', '26vh']} border='1.10733px solid rgba(158, 158, 158, 0.7)' borderRadius='6.23432px' padding='5px'>
               <Heading as="h1" size="sm" margin="8px 18px">Picking</Heading>
-              <Flex alignItems="center" justifyContent={['space-between']}margin="16px 18px">
-                <Heading as="h1" fontSize="2xl">364</Heading>
+              <Flex alignItems="center" justifyContent={['space-between']} margin="16px 18px">
+                <Heading as="h1" fontSize="2xl">{getQuantidadePedidosErro("PICKING")}</Heading>
                 <Image src={img} width="20px" height="20px" />
               </Flex>
             </Box>
