087fccd86538dde1db60948d651635f10840c107
Hamilton <hamilton.gomes8@hotmail.com>
2023-06-08 20:40:48 -0300

    feat: chart by sales channels

--- front/src/pages/Estatisticas/index.jsx
+++ front/src/pages/Estatisticas/index.jsx
@@ -2,24 +2,26 @@ import Navbar from '../../components/Navbar'
 import Visitantes from '../../components/Visitantes'
 import ChartSalesGroup from '../../components/ChartSalesGroup/ChartSalesGroup';
 import { Flex, Box, Center } from '@chakra-ui/react'
+import ChartSalesChannels from "../../components/ChartSalesChannels";
 
 const Estatisticas = () => {
   return (
     <>
       <Navbar />
-        <Flex Flex w='100%' justifyContent="Center" p='1rem'>
+      <Flex Flex w='100%' justifyContent="Center" p='1rem'>
         <Center w='1280px'>
           <Flex w='100%' justifyContent="space-between" p='1rem'>
             <Box w='70%'>
               <ChartSalesGroup />
             </Box>
             <Box w='29%' >
-            <Visitantes />
+              <Visitantes />
+              <ChartSalesChannels />
             </Box>
           </Flex>
         </Center>
 
-        </Flex>
+      </Flex>
     </>
   )
 }
